6df4650ba107d1cd4aa35e7d9c631d7a836bdfd3
jjmmnd <rnastery1@gmail.com>
2023-11-01 00:26:34 +0900

    패턴 구현 완료

--- Assets/Script/Game/Enemy/Pattern/PatternC.cs
+++ Assets/Script/Game/Enemy/Pattern/PatternC.cs
@@ -8,48 +8,40 @@ namespace Script.Game.Enemy
     /// </summary>
     public class PatternC : Pattern
     {
-        public PatternC() : base(8)
+        public PatternC() : base(3)
         {
   
         }
         public override IEnumerable<float> NextAction(Professor pf, Player.Player p)
         {
             bool isLeft = Random.Range(0,2) == 1;
-            Vector2 dir;
             
             //출첵 이후 교수위치가 Left
             if (isLeft) 
             {
-                //dir = Vector2.right;
-                //pf.tpLeft();
                 pf.facing = Direction.Left;
                 pf.tpRight();
-                yield return 0.2f;
+                yield return 0.375f;
                 pf.tpRightUp();
                 pf.출첵();
-                yield return 0.2f;
+                yield return 0.5f;
                 pf.facing = Direction.Right;
                 pf.tpLeft();
             }
             else
             {
-                //dir =Vector2.left;
-                //pf.tpRight();
                 pf.facing = Direction.Right;
                 pf.tpLeft();
-                yield return 0.2f;
+                yield return 0.375f;
                 pf.tpLeftUp();
                 pf.출첵();
-                yield return 0.2f;
+                yield return 0.5f;
                 pf.facing = Direction.Left;
                 pf.tpRight();
             }
-            yield return 0.375f;
-
-        
-            //pf.출첵(isLeft)
+            yield return 0.175f;
             pf.과제();
-            yield return 0.375f;
+            yield return 0.175f;
             pf.과제();
             
             yield return 1.0f;
