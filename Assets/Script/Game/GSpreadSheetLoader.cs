144b3e881c4bc575b290f1f1c0e7ea774174dd2e
machamy <macham2001@gmail.com>
2023-11-05 01:51:49 +0900

    구글시트 정보불러오기

---
+++ Assets/Script/Game/GSpreadSheetLoader.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GSpreadSheetLoader : MonoBehaviour
+{
+    private static GSpreadSheetLoader instance;
+
+    // https://docs.google.com/spreadsheets/d/13y3xJdLwsUohL-XTO-2nYJTsXzRg0PQglyOjY2UEQfQ/edit?usp=sharing
+    public string key;
+    public string range;
+    private string rawData;
+    private string rawRange;
+    private Dictionary<string, string[]> data;
+    void Start()
+    {
+        data = new Dictionary<string, string[]>();
+        key = key.Split("edit")[0];
+        key += "gviz/tq?tqx=out:csv&sheet=DB";
+        StartCoroutine(DownloadData("A1:B1",false));
+    }
+    
+    /// <summary>
+    /// 구글드라이브에서 데이터를 받아옴
+    /// </summary>
+    /// <param name="range"></param>
+    /// <returns></returns>
+    IEnumerator DownloadData(string range, bool isData)
+    {
+        string url = key + "&range=" + range;
+        
+        Debug.Log("[GSpreadSheetLoader::UpdateData] url : "+url);
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+            if (www.isDone)
+                rawData = www.downloadHandler.text;
+            else
+                rawData = "";
+        }
+
+        if (!isData)
+        {
+            rawRange = rawData.Split(',')[1];
+            StartCoroutine(DownloadData("B3:I"+rawRange.Replace("\"",""),true));
+            
+        }
+        else
+        {
+            UpdateData();
+        }
+    }
+
+    /// <summary>
+    /// rawdata를 dict에 정리
+    /// </summary>
+    void UpdateData()
+    {
+        string[] rows = rawData.Split('\n');
+        for(int i = 0; i < rows.Length; i++)
+        {
+            var cols = Array.ConvertAll(rows[i].Split(','),(str) => (str.Replace("\"","")));
+            if (cols.Count() == 0)
+                continue;
+            Debug.Log(cols[0]);
+            int length = int.Parse(cols[1]);
+            string[] arr = new string[length];
+            Array.Copy(cols,2,arr,0,length);
+            data.Add(cols[0],arr);
+        }
+        foreach (var VARIABLE in data)
+        {
+            Debug.Log(VARIABLE);
+        }
+    }
+    
+
+    string[] Get(string key)
+    {
+        return data[key];
+    }
+    string GetOne(string key)
+    {
+        return data[key][0];
+    }
+}
