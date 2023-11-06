21cd01da8ccfb0513aaf9cf89c07787b1302e83f
machamy <macham2001@gmail.com>
2023-11-07 08:18:53 +0900

    프레임 60고정

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -10,7 +10,7 @@ public class TitleManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
