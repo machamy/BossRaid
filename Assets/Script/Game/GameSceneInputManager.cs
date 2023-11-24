9700fc0a7f24e3947877f3b63c282e5b8e2da305
jjmmnd <rnastery1@gmail.com>
2023-11-24 23:18:23 +0900

    퍼즈 화면 수정

--- Assets/Script/Game/GameSceneInputManager.cs
+++ Assets/Script/Game/GameSceneInputManager.cs
@@ -1,18 +1,24 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Script.Game
 {
     public class GameSceneInputManager : BaseInputManager
     {
+        [SerializeField] public GameObject PauseMenu;
+        
+        public bool IsPause { get; private set; } = false;
+
+
         // Start is called before the first frame update
         private void Start()
         {
-            //base.Start();
+            base.Start();
         }
 
         protected override void Update()
         {
-            //base.Update();
+            base.Update();
         }
 
         protected override void OnHome()
@@ -22,12 +28,49 @@ namespace Script.Game
 
         protected override void OnEscape()
         {
-            SceneManager.LoadScene("Scenes/TitleScreen");
+            //SceneManager.LoadScene("Scenes/TitleScreen");
+            if (!IsPause)
+            {
+                ToPause();
+            }
+            else
+            {
+                Resume();
+            }
         }
 
         protected override void OnMenu()
         {
             
+        }
+
+        public void ToPause()
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0f; 
+            IsPause = true;
+        }
+
+        public void Resume()
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1f; 
+            IsPause = false;
+        }
+
+        public void Restart()
+        {
+
+        }
+
+        public void Help()
+        {
+            //UI 띄우기
+        }
+
+        public void Tomain()
+        {
+            SceneManager.LoadScene("Scenes/TitleScreen");
         }
     }
 }
