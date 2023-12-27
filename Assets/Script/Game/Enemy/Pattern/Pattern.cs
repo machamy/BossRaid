597d2f8382e8e19a296cf7a053e2f1540060c6d6
machamy <macham2001@gmail.com>
2023-12-28 06:13:41 +0900

    출석체크 좌우 함수 추가

--- Assets/Script/Game/Enemy/Pattern/Pattern.cs
+++ Assets/Script/Game/Enemy/Pattern/Pattern.cs
@@ -25,6 +25,30 @@ namespace Script.Game.Enemy
         {
             yield return 1.0f;
         }
-        
+
+        protected IEnumerable<float> 출첵L(Professor pf)
+        {
+            pf.Facing = Direction.Left;
+            pf.tpRight();
+            yield return 0.375f;
+            pf.tpRightUp();
+            pf.출첵(LeftPosStart,LeftPosEnd,AttendAmount);
+            yield return 0.5f;
+            pf.Facing = Direction.Right;
+            pf.tpLeft();
+        }
+
+        protected IEnumerable<float> 출첵R(Professor pf)
+        {
+            pf.Facing = Direction.Right;
+            pf.tpLeft();
+            yield return 0.375f;
+            pf.tpLeftUp();
+            pf.출첵(RightPosStart,RightPosEnd,AttendAmount);
+            yield return 0.5f;
+            pf.Facing = Direction.Left;
+            pf.tpRight();
+        }
     }
+    
 }
