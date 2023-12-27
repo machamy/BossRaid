7ac20c266f2bb7179279b3ef58974b188f76933c
machamy <macham2001@gmail.com>
2023-12-28 06:14:06 +0900

    패턴 E 임시 추가

---
+++ Assets/Script/Game/Enemy/Pattern/PatternE.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Game.Enemy
+{
+    /// <summary>
+    /// 플레이어 좌우에 한번 , 플레이어 위치에 한번  수직공격
+    /// </summary>
+    public class PatternE : Pattern
+    {
+        private Ground gr;
+        
+        public PatternE() : base(2)
+        {
+            gr = GameObject.FindObjectOfType<Ground>();
+        }
+        public override IEnumerable<float> NextAction(Professor pf, Player.Player p)
+        {
+            gr.퀴즈();
+            yield return 1.0f;
+        }
+    }
+}
