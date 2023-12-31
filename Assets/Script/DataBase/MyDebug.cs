2c172362aeee92487a7d9bb37f68ace359f5f442
machamy <macham2001@gmail.com>
2023-12-31 14:51:35 +0900

    디버그 옵션 추가

---
+++ Assets/Script/DataBase/MyDebug.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Global
+{
+    public class MyDebug
+    {
+        private static MyDebug instance;
+
+        public static MyDebug Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new MyDebug();
+                    instance.init(DB.DEBUG);
+                }
+                return instance;
+            }
+        }
+
+        private Dictionary<string, string> data = new Dictionary<string, string>();
+
+        public void init(string[] rawData)
+        {
+            foreach (var raw in rawData)
+            {
+                if(raw.Length < 2)
+                    continue;
+                if (!raw.Contains(" "))
+                {
+                    data[raw.ToLower()] = "1";
+                    continue;
+                }
+                var arr = raw.Split(" ");
+                data[arr[0].ToLower()] = arr[1];
+            }
+        }
+        
+        public int getInt(string name)
+        {
+            string raw;
+            if (!data.TryGetValue(name.ToLower(), out raw))
+                return -1;
+            return Int32.Parse(raw);
+        }
+
+        public bool getBool(string name)
+        {
+            string raw;
+            if (!data.TryGetValue(name.ToLower(), out raw))
+                return false;
+            return Int32.Parse(raw) != -1;
+        }
+    }
+}
