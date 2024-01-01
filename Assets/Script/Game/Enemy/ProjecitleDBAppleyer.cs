58d6d2b8cd39809c39bfee07c00a530886a8273d
machamy <macham2001@gmail.com>
2024-01-01 11:48:41 +0900

    투사체 DB적용 클래스 새로 작성

---
+++ Assets/Script/Game/Enemy/ProjecitleDBAppleyer.cs
@@ -0,0 +1,40 @@
+﻿using Script.Global;
+using UnityEngine;
+
+namespace Script.Game.Enemy.Projectile
+{
+    public class ProjecitleDBAppleyer : MonoBehaviour, DBUser
+    {
+        public GameObject attend, practice, team, fire;
+        
+        
+        public void ApplyDBdata()
+        {
+            GameObject[] prefebs = new [] { attend, practice, team };
+            var dbs = new [] { DB.AttendProjectile, DB.PracticeProjectile, DB.TeamProjectile };
+            for (int i = 0; i< 3;i ++)
+            {
+                var prjt = prefebs[i].GetComponent<Game.Projectile.Projectile>();
+                var datas = dbs[i];
+                if(datas == null)
+                    continue;
+                prjt.speed = float.Parse(datas[0]);
+                prjt.parringScore = int.Parse(datas[1]);
+                prjt.damage = int.Parse(datas[2]);
+            }
+
+            FireWall fire = this.fire.GetComponent<FireWall>();
+            
+            if (DB.FireWall != null)
+            {
+                fire.lifetime = float.Parse(DB.FireWall[0]);
+                fire.damage = int.Parse(DB.FireWall[1]);
+            }
+            if (DB.FireWallAttack != null)
+            {
+                fire.warnTime = float.Parse(DB.FireWallAttack[0]);
+            }
+            
+        }
+    }
+}
